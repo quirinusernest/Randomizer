b0e7e4b4c1150ad5292f5e3e5afc52e2a45ede3b
quirinusernest <rinusernest@gmail.com>
2020-07-24 23:29:26 +0700

    Ujicoba Android Studio

---
+++ app/src/main/java/teamtreehouse/com/MainActivity.kt
@@ -0,0 +1,27 @@
+package teamtreehouse.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random().nextInt(seekBar.progress) + 1
+            resultTextView.text = rand.toString()
+
+        }
+    }
+}
